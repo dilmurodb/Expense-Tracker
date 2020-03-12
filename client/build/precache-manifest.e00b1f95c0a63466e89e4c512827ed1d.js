453b5595112f8fb5a9bcc67cb659d09c0e742095
bukharov <dilmurod.bukharov@gmail.com>
2020-03-11 20:35:55 -0400

    deleted the previous repository, created a new one and pasted all the code

---
+++ client/build/precache-manifest.e00b1f95c0a63466e89e4c512827ed1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8789dbab8ae63f29af3b8b35b9af30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "292d38d5d6fb598b1fc0",
+    "url": "/static/css/main.caf1db2f.chunk.css"
+  },
+  {
+    "revision": "e4245dd5c39eb1fcc4c3",
+    "url": "/static/js/2.2f7a2ab1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.2f7a2ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "292d38d5d6fb598b1fc0",
+    "url": "/static/js/main.207b91b9.chunk.js"
+  },
+  {
+    "revision": "07ae20d39d5cc9f55d44",
+    "url": "/static/js/runtime-main.fce36740.js"
+  }
+]);
